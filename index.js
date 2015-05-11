f18a4b83f0358dcd81f9c04b35debf272780c796
Sigurd Fosseng <sigurd@fosseng.net>
2015-05-11 23:24:54 +0200

    relative paths when myth({sourcemap: true})

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var objectAssign = require('object-assign');
 var myth = require('myth');
+var path = require('path');
 
 module.exports = function (options) {
 	options = options || {};
@@ -17,11 +18,13 @@ module.exports = function (options) {
 			cb(new gutil.PluginError('gulp-myth', 'Streaming not supported'));
 			return;
 		}
-
-		options = objectAssign({}, options, {source: file.path});
+		options = objectAssign({}, options, {source: path.basename(file.path)});
 
 		try {
+			var cwd = process.cwd();
+			process.chdir(path.dirname(file.path));
 			file.contents = new Buffer(myth(file.contents.toString(), options));
+			process.chdir(cwd);
 			this.push(file);
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-myth', err, {fileName: file.path}));
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
 var assert = require('assert');
 var gutil = require('gulp-util');
+var convert = require('convert-source-map');
 var myth = require('./');
 
 it('should postprocess CSS using Myth', function (cb) {
@@ -29,4 +30,24 @@ it('should support Source Map', function (cb) {
 		path: __dirname + '/fixture.css',
 		contents: new Buffer(':root{--green:#a6c776;}a{color:var(--green);}')
 	}));
+});
+
+it('should support relative paths', function(cb) {
+	var stream = myth({sourcemap: true});
+
+	stream.on('data', function (file) {
+		var content = file.contents.toString();
+		var sourcemap = convert.fromSource(content).sourcemap;
+		sourcemap.sources.forEach(function(source){
+			var startsWithBasename = source.indexOf(__dirname) === 0;
+			assert(!startsWithBasename, "expected [" + source +"] to be relative");
+		});
+		cb();
+	});
+
+	stream.write(new gutil.File({
+		path: __dirname + '/testdata/test.css',
+		contents: new Buffer('@import url("test-imported.css");')
+	}));
+
 });
